ea2b34eff72fb9a5b2d8bea356b12049b6ae3e5b
Deva <dvrharsha@gmail.com>
2024-10-25 13:08:53 -0400

    updated

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -4,3 +4,11 @@ resource "azurerm_resource_group" "rg" {
   location = each.value["location"]
   tags     = each.value["tags"]
 }
+
+output "resource_group_ids" {
+  value = { for r in azurerm_resource_group.rg : r.name => r.id }
+}
+
+output "rg_names" {
+  value = [for r in azurerm_resource_group.rg : r.name]
+}
--- modules/resourcegroup/output.tf
+++
@@ -1,7 +0,0 @@
-output "resource_group_ids" {
-  value = { for r in azurerm_resource_group.rg : r.name => r.id }
-}
-
-output "rg_names" {
-  value = [for r in azurerm_resource_group.rg : r.name]
-}
